40d26ce8f64eb1db6b63d1870cd1aa1617a701c8
Leslie Lopez <leslie2515lopez@gmail.com>
2022-12-14 23:05:39 -0600

    Sign out sagas

--- src/store/user/user.reducer.js
+++ src/store/user/user.reducer.js
@@ -14,6 +14,7 @@ export const userReducer = (state = USER_INITIAL_STATE, action = {}) => {
       return { ...state, currentUser: payload };
     case USER_ACTION_TYPES.SIGN_OUT_SUCCESS:
       return { ...state, currentUser: null };
+    case USER_ACTION_TYPES.SIGN_OUT_FAILED:
     case USER_ACTION_TYPES.SIGN_IN_FAILED:
     case USER_ACTION_TYPES.SIGN_UP_FAILED:
       return { ...state, error: payload };
