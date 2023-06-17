0d36d84c68dbf5dd3a326f31e2b256e48a47f0a0
Leslie Lopez <leslie2515lopez@gmail.com>
2023-06-16 23:47:04 -0600

    Updated sign up form component's css

--- src/components/sign-up-form/sign-up-form.styles.tsx
+++ src/components/sign-up-form/sign-up-form.styles.tsx
@@ -5,6 +5,12 @@ export const SignUpContainer = styled.div`
   flex-direction: column;
   width: 380px;
 
+  @media screen and (max-width: 800px) {
+    position: relative;
+    bottom: 390px;
+    left: 420px;
+  }
+
   h2 {
     margin: 10px 0;
     color: #d97d0c;
