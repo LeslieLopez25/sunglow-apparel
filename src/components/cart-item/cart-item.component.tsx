a6ce5e80045558eeaa473881f565c9b25dd0f9e0
Leslie Lopez <leslie2515lopez@gmail.com>
2025-05-06 23:24:51 -0600

    Added comments to cart item component

--- src/components/cart-item/cart-item.component.tsx
+++ src/components/cart-item/cart-item.component.tsx
@@ -8,6 +8,7 @@ type CartItemProps = {
   cartItem: TCartItem;
 };
 
+// Renders an individual cart item showing the product image, name, quantity, and price
 const CartItem: FC<CartItemProps> = ({ cartItem }) => {
   const { name, imageUrl, price, quantity } = cartItem;
   return (
