55b627c3aa77fac9ad9599ad548decf8ae78b295
Leslie Lopez <leslie2515lopez@gmail.com>
2025-05-07 22:09:10 -0600

    Added comments to form input component

--- src/components/form-input/form-input.component.tsx
+++ src/components/form-input/form-input.component.tsx
@@ -4,6 +4,7 @@ import { FormInputLabel, Input, Group } from "./form-input.styles";
 
 type FormInputProps = { label: string } & InputHTMLAttributes<HTMLInputElement>;
 
+// Renders a styled input field with a label that shrinks when the input has a value
 const FormInput: FC<FormInputProps> = ({ label, ...otherProps }) => {
   return (
     <Group>
