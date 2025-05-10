33bc934bcad94a622d1a1356c6b725d87689ac1f
Leslie Lopez <leslie2515lopez@gmail.com>
2025-05-09 21:29:07 -0600

    Added comments to category actions

--- src/store/categories/category.action.ts
+++ src/store/categories/category.action.ts
@@ -7,24 +7,29 @@ import {
   withMatcher,
 } from "../../utils/reducer/reducer.utils";
 
+// Action type for starting the category fetch process
 export type FetchCategoriesStart =
   Action<CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_START>;
 
+// Action type for successfully fetched categories with payload
 export type FetchCategoriesSuccess = ActionWithPayload<
   CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_SUCCESS,
   Category[]
 >;
 
+// Action type for a failed category fetch with error payload
 export type FetchCategoriesFailed = ActionWithPayload<
   CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_FAILED,
   Error
 >;
 
+// Action creator to signal the start of fetching categories
 export const fetchCategoriesStart = withMatcher(
   (): FetchCategoriesStart =>
     createAction(CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_START)
 );
 
+// Action creator to handle successful category fetch
 export const fetchCategoriesSuccess = withMatcher(
   (categoriesArray: Category[]): FetchCategoriesSuccess =>
     createAction(
@@ -33,6 +38,7 @@ export const fetchCategoriesSuccess = withMatcher(
     )
 );
 
+// Action creator to handle failure during category fetch
 export const fetchCategoriesFailed = withMatcher(
   (error: Error): FetchCategoriesFailed =>
     createAction(CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_FAILED, error)
