253e870ede10f7c53777da46b484213453f8d156
Leslie Lopez <leslie2515lopez@gmail.com>
2025-05-09 23:48:11 -0600

    Added comments to category selector

--- src/store/categories/category.selector.ts
+++ src/store/categories/category.selector.ts
@@ -4,14 +4,17 @@ import { RootState } from "../store";
 import { CategoriesState } from "./category.reducer";
 import { CategoryMap } from "./category.types";
 
+// Base selector to get the categories slice from the root state
 const selectCategoryReducer = (state: RootState): CategoriesState =>
   state.categories;
 
+// Selector to get the raw categories array
 export const selectCategories = createSelector(
   [selectCategoryReducer],
   (categoriesSlice) => categoriesSlice.categories
 );
 
+// Selector to transform categories array into a map of title -> items
 export const selectCategoriesMap = createSelector(
   [selectCategories],
   (categories) =>
@@ -22,6 +25,7 @@ export const selectCategoriesMap = createSelector(
     }, {} as CategoryMap)
 );
 
+// Selector to get the loading state of categories
 export const selectCategoriesIsLoading = createSelector(
   [selectCategoryReducer],
   (categoriesSlice) => categoriesSlice.isLoading
