19f2c1689096b8192b74373442dd81d392e02dfe
Leslie Lopez <leslie2515lopez@gmail.com>
2025-05-09 21:10:48 -0600

    Added comments to cart selector

--- src/store/cart/cart.selector.ts
+++ src/store/cart/cart.selector.ts
@@ -4,18 +4,22 @@ import { RootState } from "../store";
 
 import { CartState } from "./cart.reducer";
 
+// Base selector to get the cart slice from the overall Redux state
 const selectCartReducer = (state: RootState): CartState => state.cart;
 
+// Selector to get the boolean indicating whether the cart dropdown is open
 export const selectIsCartOpen = createSelector(
   [selectCartReducer],
   (cart) => cart.isCartOpen
 );
 
+// Selector to get the array of items in the cart
 export const selectCartItems = createSelector(
   [selectCartReducer],
   (cart) => cart.cartItems
 );
 
+// Selector to calculate the total cost of all items in the cart
 export const selectCartTotal = createSelector([selectCartItems], (cartItems) =>
   cartItems.reduce(
     (total, cartItem) => total + cartItem.quantity * cartItem.price,
@@ -23,6 +27,7 @@ export const selectCartTotal = createSelector([selectCartItems], (cartItems) =>
   )
 );
 
+// Selector to calculate the total quantity of items in the cart
 export const selectCartCount = createSelector([selectCartItems], (cartItems) =>
   cartItems.reduce((total, cartItem) => total + cartItem.quantity, 0)
 );
