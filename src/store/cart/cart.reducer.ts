d1dfe7d2c225ccebeea8093523bf91dcd2c1a88c
Leslie Lopez <leslie2515lopez@gmail.com>
2025-05-09 20:54:35 -0600

    Added comments to cart reducer

--- src/store/cart/cart.reducer.ts
+++ src/store/cart/cart.reducer.ts
@@ -3,16 +3,20 @@ import { AnyAction } from "redux";
 import { CartItem } from "./cart.types";
 import { setIsCartOpen, setCartItems } from "./cart.action";
 
+// Type for the cart state in the Redux store
 export type CartState = {
   readonly isCartOpen: boolean;
   readonly cartItems: CartItem[];
 };
 
+// Initial state for the cart
 export const CART_INITIAL_STATE: CartState = {
   isCartOpen: false,
   cartItems: [],
 };
 
+// Reducer function to handle cart-related actions
+// Updates cart state based on the type of action dispatched
 export const cartReducer = (state = CART_INITIAL_STATE, action: AnyAction) => {
   if (setIsCartOpen.match(action)) {
     return {
@@ -21,6 +25,7 @@ export const cartReducer = (state = CART_INITIAL_STATE, action: AnyAction) => {
     };
   }
 
+  // Handles setting the array of items in the cart
   if (setCartItems.match(action)) {
     return {
       ...state,
