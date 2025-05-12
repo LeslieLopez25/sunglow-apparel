468e0a64de446867a60b1b95bc04fe6001005dab
Leslie Lopez <leslie2515lopez@gmail.com>
2025-05-11 21:12:13 -0600

    Added comments to user selector

--- src/store/user/user.selector.ts
+++ src/store/user/user.selector.ts
@@ -4,8 +4,10 @@ import { RootState } from "../store";
 
 import { UserState } from "./user.reducer";
 
+// Selector to get the entire user state slice from the Redux store
 export const selectUserReducer = (state: RootState): UserState => state.user;
 
+// Selector to get only the currentUser object from the user state slice
 export const selectCurrentUser = createSelector(
   selectUserReducer,
   (user) => user.currentUser
