a28022346c08d03bdb6c1d8e9bd91ee6c50c80fe
Leslie Lopez <leslie2515lopez@gmail.com>
2025-05-11 20:56:46 -0600

    Added comments to user reducer

--- src/store/user/user.reducer.ts
+++ src/store/user/user.reducer.ts
@@ -10,27 +10,33 @@ import {
 
 import { UserData } from "../../utils/firebase/firebase.utils";
 
+// Defines the shape of the user state in Redux
 export type UserState = {
   readonly currentUser: UserData | null;
   readonly isLoading: boolean;
   readonly error: Error | null;
 };
 
+// Initial state when the app starts
 const INITIAL_STATE: UserState = {
   currentUser: null,
   isLoading: false,
   error: null,
 };
 
+// Reducer that handles user-related actions to update the state
 export const userReducer = (state = INITIAL_STATE, action: AnyAction) => {
+  //  When user signs in successfully, set currentUser to the returned payload
   if (signInSuccess.match(action)) {
     return { ...state, currentUser: action.payload };
   }
 
+  // When user signs out successfully, clear the currentUser
   if (signOutSuccess.match(action)) {
     return { ...state, currentUser: null };
   }
 
+  // If any authentication action fails, store the error
   if (
     signInFailed.match(action) ||
     signUpFailed.match(action) ||
