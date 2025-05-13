ee708d81a07865bbe67d8ecaa74508e5d34539d2
Leslie Lopez <leslie2515lopez@gmail.com>
2025-05-13 00:00:42 -0600

    Added comments to reducer utils

--- src/utils/reducer/reducer.utils.ts
+++ src/utils/reducer/reducer.utils.ts
@@ -1,18 +1,23 @@
+// Utility functions for creating and type-checking Redux actions
 import { AnyAction } from "redux";
 
+// Enhances an action creator with a `match` method and typed `type` property
 type Matchable<AC extends () => AnyAction> = AC & {
   type: ReturnType<AC>["type"];
   match(action: AnyAction): action is ReturnType<AC>;
 };
 
+// Overload for no-argument action creators
 export function withMatcher<AC extends () => AnyAction & { type: string }>(
   actionCreator: AC
 ): Matchable<AC>;
 
+// Overload for argument-based action creators
 export function withMatcher<
   AC extends (...args: any[]) => AnyAction & { type: string }
 >(actionCreator: AC): Matchable<AC>;
 
+// Implementation of `withMatcher`
 export function withMatcher(actionCreator: Function) {
   const type = actionCreator().type;
   return Object.assign(actionCreator, {
@@ -23,25 +28,30 @@ export function withMatcher(actionCreator: Function) {
   });
 }
 
+// Action with payload type
 export type ActionWithPayload<T, P> = {
   type: T;
   payload: P;
 };
 
+// Action without payload
 export type Action<T> = {
   type: T;
 };
 
+// Overload for action with payload
 export function createAction<T extends string, P>(
   type: T,
   payload: P
 ): ActionWithPayload<T, P>;
 
+// Overload for action without payload
 export function createAction<T extends string>(
   type: T,
   payload: void
 ): Action<T>;
 
+// Implementation of `createAction`
 export function createAction<T extends string, P>(type: T, payload: P) {
   return { type, payload };
 }
